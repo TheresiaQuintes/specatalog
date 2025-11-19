62a851f508d5dcaf5db917c61a1ca206e401f3ad
Theresia Quintes <theresia.quintes@physchem.uni-freiburg.de>
2025-11-19 11:05:53 +0100

    create-datei getrennt

---
+++ models/creation_pydantic_measurements.py
@@ -0,0 +1,63 @@
+from pydantic import BaseModel, Field, ConfigDict, field_validator
+from typing import Type, Optional
+import models.measurements as ms
+import models.allowed_values as av
+import datetime
+
+class MeasurementModel(BaseModel):
+    molecular_id: int
+    temperature: float = Field(..., gt=0)
+    solvent: av.Solvents
+    concentration: Optional[str]=None
+    date: datetime.date
+    measured_by : av.Names
+    location: Optional[str]=None
+    device: Optional[av.Devices]=None
+    series: Optional[str]=None
+    path: str
+    corrected: bool
+    evaluated: bool
+
+    model_config = ConfigDict(extra="forbid", validate_assignment=True)
+
+
+class CWEPRModel(MeasurementModel):
+    measurement_class: Type=ms.CWEPR
+    @field_validator("measurement_class")
+    def check_grp(cls, v):
+        if v is not ms.CWEPR:
+            raise ValueError("model_class darf nicht geändert werden")
+        return v
+
+    frequency_band: av.FrequencyBands
+    attenuation: float
+
+
+class TREPRModel(MeasurementModel):
+    measurement_class: Type=ms.TREPR
+    @field_validator("measurement_class")
+    def check_grp(cls, v):
+        if v is not ms.TREPR:
+            raise ValueError("model_class darf nicht geändert werden")
+        return v
+
+    frequency_band: av.FrequencyBands
+    excitation_wl: float
+    excitation_energy: Optional[float]=None
+    attenuation: float
+    number_of_scans: Optional[int]=None
+    repetitionrate: Optional[float]=None
+    mode: Optional[str]=None
+
+
+class PulseEPRModel(MeasurementModel):
+    measurement_class: Type=ms.PulseEPR
+    @field_validator("measurement_class")
+    def check_grp(cls, v):
+        if v is not ms.PulseEPR:
+            raise ValueError("model_class darf nicht geändert werden")
+        return v
+
+    frequency_band: Optional[av.FrequencyBands]=None
+    dsc_path: str
+    pulse_experiment: av.PulseExperiments
---
+++ models/creation_pydantic_molecules.py
@@ -0,0 +1,76 @@
+from pydantic import BaseModel, ConfigDict, field_validator, computed_field
+from typing import Type, Optional
+import models.molecules as mol
+import models.allowed_values as av
+
+
+class MoleculeModel(BaseModel):
+    molecular_formula: str
+    structural_formula: str
+    smiles: str
+    model_config = ConfigDict(extra="forbid", validate_assignment=True)
+
+class SingleMoleculeModel(MoleculeModel):
+    model_class: Type=mol.Single
+    @field_validator("model_class")
+    def check_grp(cls, v):
+        if v is not mol.Single:
+            raise ValueError("model_class darf nicht geändert werden")
+        return v
+
+    name: str
+    additional_info: Optional[str]=None
+
+
+class RPModel(MoleculeModel):
+    model_class: Type=mol.RP
+    @field_validator("model_class")
+    def check_grp(cls, v):
+        if v is not mol.RP:
+            raise ValueError("model_class darf nicht geändert werden")
+        return v
+
+    radical_1: av.Radicals
+    linker: av.Linker
+    radical_2: av.Radicals
+
+    @computed_field
+    @property
+    def name(self) -> str:
+        return f"{self.radical_1.value}-{self.linker.value}-{self.radical_2.value}"
+
+
+class TDPModel(MoleculeModel):
+    model_class: Type=mol.TDP
+    @field_validator("model_class")
+    def check_grp(cls, v):
+        if v is not mol.TDP:
+            raise ValueError("model_class darf nicht geändert werden")
+        return v
+
+    doublet: av.Doublets
+    linker: av.Linker
+    chromophore: av.Chromophores
+
+    @computed_field
+    @property
+    def name(self) -> str:
+        return f"{self.chromophore.value}-{self.linker.value}-{self.doublet.value}"
+
+
+class TTPModel(MoleculeModel):
+    model_class: Type=mol.TTP
+    @field_validator("model_class")
+    def check_grp(cls, v):
+        if v is not mol.TTP:
+            raise ValueError("model_class darf nicht geändert werden")
+        return v
+
+    triplet_1: av.Chromophores
+    linker: av.Linker
+    triplet_2: av.Chromophores
+
+    @computed_field
+    @property
+    def name(self) -> str:
+        return f"{self.triplet_1.value}-{self.linker.value}-{self.triplet_2.value}"
