8f7e14f4c91add89fd384d5a753dc63a86fb72f7
Theresia Quintes <theresia.quintes@physchem.uni-freiburg.de>
2025-12-04 13:04:20 +0100

    conf.py bearbeitet

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,11 +63,8 @@ latex_engine = "xelatex"
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_theme_options = {
-    "prev_next_buttons_location": "both",
 }
 html_codeblock_linenos_style = "table"
-html_static_path = ['_static']
 html_css_files = [
     'custom.css',
 ]
@@ -76,8 +73,8 @@ html_css_files = [
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_baseurl = "https://theresiaquintes.github.io/teacups/"
 html_theme_options = {
+    "prev_next_buttons_location": "both",
     "navigation_with_keys": True,
 }
 
