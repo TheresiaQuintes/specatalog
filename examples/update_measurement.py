47ace18be7f1cf78a7badfe670051764e385340a
Theresia Quintes <theresia.quintes@physchem.uni-freiburg.de>
2025-12-19 12:33:29 +0100

    Update of Documentation

--- examples/update_measurement.py
+++
@@ -1,8 +0,0 @@
-from specatalog.crud_db import read as r
-from specatalog.crud_db import update as up
-
-measurement = r.run_query(r.MeasurementFilter(id=1))[0]
-
-updates = up.CWEPRUpdate(frequency_band="Q", temperature=80, solvent="toluene", evaluated=True)
-
-up.update_model(measurement, updates)
