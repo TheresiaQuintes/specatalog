4b56209c2e42094f1c795a3d305b8ab63f7d101c
Theresia Quintes <theresia.quintes@physchem.uni-freiburg.de>
2026-05-21 10:27:45 +0200

    Tests laufen auch in CI-Umgebung, Fake-Home/Defaults/allowed_values wird in conftest.py/pytest_configure() erstellt

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -6,6 +6,48 @@ import json
 from pathlib import Path
 import tempfile
 
+
+TEST_ROOT = None
+THIS_DIR = Path(__file__).resolve().parent
+PROJECT_ROOT = THIS_DIR.parent
+
+
+def pytest_configure():
+    global TEST_ROOT
+    # create temp root
+    TEST_ROOT = Path(tempfile.mkdtemp())
+
+    # fake HOME
+    fake_home = TEST_ROOT
+
+    # ~/.specatalog
+    specatalog_dir = fake_home / ".specatalog"
+    specatalog_dir.mkdir(parents=True)
+
+    Path.home = lambda: fake_home
+
+    # workspace
+    workspace = TEST_ROOT / "archive_specatalog"
+    workspace.mkdir()
+
+    # provide allowed_values.py
+    source_file = (
+        PROJECT_ROOT / "specatalog" / "helpers" / "allowed_values_not_adapted.py"
+    )
+    shutil.copy(Path(source_file), workspace / "allowed_values.py")
+
+    # fake defaults.json
+    defaults = {
+        "base_path": str(workspace),
+        "usr_name": "test",
+        "password": "test",
+        "database_url": "localhost/test",
+    }
+
+    with open(specatalog_dir / "defaults.json", "w") as f:
+        json.dump(defaults, f)
+
+
 @pytest.fixture
 def engine():
     engine = create_engine("sqlite:///:memory:")
@@ -22,6 +64,7 @@ def engine():
 @pytest.fixture
 def db_session(engine):
     from specatalog.models.base import Model
+
     Model.metadata.create_all(engine)
 
     SessionLocal = sessionmaker(bind=engine)
@@ -31,84 +74,3 @@ def db_session(engine):
 
     session.close()
     Model.metadata.drop_all(engine)
-
-
-@pytest.fixture
-def test_workspace(tmp_path, monkeypatch):
-    # fake workspace
-    workspace = tmp_path / "archive_specatalog"
-    workspace.mkdir()
-
-    # allowed_values.py bereitstellen
-    shutil.copy(
-        Path("specatalog/helpers/allowed_values_not_adapted.py"),
-        workspace / "allowed_values.py",
-    )
-
-    # fake defaults.json
-    defaults_dir = Path.home() / ".specatalog" / "defaults.json"
-    defaults_dir.mkdir()
-
-    defaults = {
-        "base_path": str(workspace),
-        "usr_name": "test",
-        "password": "test",
-        "database_url": "localhost/test",
-    }
-
-    with open(defaults_dir / "defaults.json", "w") as f:
-        json.dump(defaults, f)
-
-    # monkeypatch.setattr(Path, "home", lambda: tmp_path)
-
-    yield workspace
-
-TEST_ROOT = None
-THIS_DIR = Path(__file__).resolve().parent
-PROJECT_ROOT = THIS_DIR.parent
-
-def pytest_configure():
-
-    global TEST_ROOT
-
-    # temporäres root erzeugen
-    TEST_ROOT = Path(tempfile.mkdtemp())
-
-    # fake HOME
-    fake_home = TEST_ROOT
-
-    # ~/.specatalog
-    specatalog_dir = fake_home / ".specatalog"
-    specatalog_dir.mkdir(parents=True)
-
-    # workspace
-    workspace = TEST_ROOT / "archive_specatalog"
-    workspace.mkdir()
-
-    # allowed_values.py bereitstellen
-    source_file = (
-            PROJECT_ROOT
-            / "specatalog"
-            / "helpers"
-            / "allowed_values_not_adapted.py"
-    )
-    shutil.copy(
-        Path(source_file),
-        workspace / "allowed_values.py"
-    )
-
-    # fake defaults.json
-    defaults = {
-        "base_path": str(workspace),
-        "usr_name": "test",
-        "password": "test",
-        "database_url": "localhost/test"
-    }
-
-    with open(specatalog_dir / "defaults.json", "w") as f:
-        json.dump(defaults, f)
-
-    # HOME überschreiben
-    import pathlib
-
-    pathlib.Path.home = lambda: fake_home
