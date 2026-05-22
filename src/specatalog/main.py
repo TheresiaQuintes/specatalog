488d2777f87bf6f4374d71e85c3de50f3c5ecd60
Theresia Quintes <theresia.quintes@physchem.uni-freiburg.de>
2026-05-22 10:34:08 +0200

    fixing ruff-error

--- src/specatalog/main.py
+++ src/specatalog/main.py
@@ -87,4 +87,5 @@ try:
     ALLOWED_VALUES = load_allowed_values(BASE_PATH / "allowed_values.py")
 except FileNotFoundError:
     print("Please run postinstall to generate your own allowed_values.py.")
-    import specatalog.helpers.allowed_values_not_adapted as ALLOWED_VALUES
+    import specatalog.helpers.allowed_values_not_adapted as module
+    ALLOWED_VALUES = module
