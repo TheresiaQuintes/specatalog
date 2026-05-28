a08a13c1c07788d680c16d8feb83b2800a9a9cce
Theresia Quintes <theresia.quintes@physchem.uni-freiburg.de>
2026-05-28 14:37:32 +0200

    Weitere tests für Measurement-Modelle hinzugefügt

--- src/tests/test_models_measurements.py
+++ src/tests/test_models_measurements.py
@@ -1,4 +1,5 @@
 import pytest
+from sqlalchemy.exc import IntegrityError
 
 import specatalog.models.measurements as mmes
 from datetime import date
@@ -72,77 +73,148 @@ def test_set_polymorphic_identity(db_session, entry_factory, molecule_instance, 
 
 
 
-    """
-    @pytest.mark.parametrize(
-    "name1,name2,struct1,struct2",
-    [
-        # same name
-        ("Mol", "Mol", "/tmp/a", "/tmp/b"),
-
-        # same structural_formula
-        ("MolA", "MolB", "/tmp/a", "/tmp/a"),
-    ],
-)
-def test_molecule_name_unique(db_session, name1, name2, struct1, struct2):
-    mol1 = mol_model.Molecule(
-        name=name1,
-        structural_formula=struct1,
-        group="base",
+def test_measurement_path_unique(db_session, molecule_instance):
+    ms1 = mmes.Measurement(
+        molecule=molecule_instance,
+        temperature=300,
+        solvent="Water",
+        date=date(2025, 5, 6),
+        measured_by="Alice",
+        path="/tmp/m1",
+        corrected=False,
+        evaluated=False
+    )
+    ms2 =  mmes.Measurement(
+        molecule=molecule_instance,
+        temperature=300,
+        solvent="Water",
+        date=date(2025, 5, 6),
+        measured_by="Alice",
+        path="/tmp/m1",
+        corrected=False,
+        evaluated=False
     )
 
-    mol2 = mol_model.Molecule(
-        name=name2,
-        structural_formula=struct2,
-        group="base",
-    )
-
-    db_session.add_all([mol1, mol2])
+    db_session.add_all([ms1, ms2])
 
     with pytest.raises(IntegrityError):
         db_session.commit()
 
 
-params = [
-    tuple(
-        None if i == none_index else "bla"
-        for i in range(11)
+
+def generate_params():
+    rows = [
+        [None if col == row else "bla" for col in range(18)]
+        for row in range(13)
+    ]
+
+    rows.extend([
+        [None if col == 13 else True for col in range(18)],
+        [None if col == 14 else True for col in range(18)],
+        [None if col == 15 else True for col in range(18)],
+    ])
+
+    rows.extend([
+        [None if col == 16 else 1.2 for col in range(18)],
+        [None if col == 17 else 1.2 for col in range(18)],
+    ])
+
+    return list(zip(*rows))
+
+
+params = generate_params()
+
+
+def make_base(molecule, **overrides):
+    base = dict(
+        molecule=molecule,
+        temperature=300,
+        solvent="Water",
+        date=date(2025, 5, 6),
+        measured_by="Alice",
+        path="/tmp/m1",
+        corrected=False,
+        evaluated=False,
     )
-    for none_index in range(11)
-]
+    base.update(overrides)
+    return base
+
+
 @pytest.mark.parametrize(
-    "a,b,c,d,e,f,g,h,i,j,k",
+    "str_a,str_b,str_c,str_d,str_e,str_f,str_g,str_h,str_i,str_j,str_k,str_l,str_m,"
+    "bool_a,bool_b,bool_c,num_a,num_b",
     params,
 )
-def test_required_values(db_session, a, b, c, d, e, f, g, h, i, j, k):
-    mol = mol_model.Molecule(
-        name=a,
-        structural_formula=b
+def test_required_values(
+    db_session,
+    molecule_instance,
+    str_a, str_b, str_c, str_d, str_e, str_f, str_g,
+    str_h, str_i, str_j, str_k, str_l, str_m,
+    bool_a, bool_b, bool_c, num_a, num_b
+):
+
+    ms = mmes.Measurement(
+        **make_base(
+            molecule_instance,
+            temperature=num_a,
+            solvent=str_a,
+            measured_by=str_b,
+            path=str_c,
+            corrected=bool_a,
+            evaluated=bool_b,
+        )
     )
 
-    rp = mol_model.RP(
-        name="bla",
-        structural_formula="bla",
-        radical_1=c,
-        radical_2=d,
-        linker=e)
+    trepr = mmes.TREPR(
+        **make_base(
+            molecule_instance,
+            frequency_band=str_d,
+            excitation_wl=num_b,
+            attenuation=str_e,
+        )
+    )
 
-    tdp = mol_model.TDP(
-        name="bla",
-        structural_formula="bla",
-        doublet=f,
-        linker=g,
-        chromophore=h)
+    cwepr = mmes.CWEPR(
+        **make_base(
+            molecule_instance,
+            frequency_band=str_f,
+            attenuation=str_g,
+        )
+    )
 
-    ttp = mol_model.TTP(
-        name="bla",
-        structural_formula="bla",
-        triplet_1=i,
-        linker=j,
-        triplet_2=k)
+    pulse = mmes.PulseEPR(
+        **make_base(
+            molecule_instance,
+            pulse_experiment=str_h,
+        )
+    )
 
-    db_session.add_all([mol, rp, tdp, ttp])
+    uvvis = mmes.UVVis(
+        **make_base(
+            molecule_instance,
+            dim_cuvette=str_i,
+        )
+    )
+
+    fluorescence = mmes.Fluorescence(
+        **make_base(
+            molecule_instance,
+            excitation=bool_c,
+            excitation_wl=str_j,
+        )
+    )
+
+    ta = mmes.TA(
+        **make_base(
+            molecule_instance,
+            timedomain=str_k,
+            excitation_energy=str_l,
+            excitation_wl=str_m,
+        )
+    )
+
+    db_session.add_all([ms, trepr, cwepr, pulse, uvvis, fluorescence, ta])
 
     with pytest.raises(IntegrityError):
         db_session.commit()
-        
-        """
+
