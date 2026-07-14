b1db8a68e66bfc5d25d5026da56981260d1ba0e7
Theresia Quintes <theresia.quintes@physchem.uni-freiburg.de>
2026-07-14 15:18:53 +0200

    Löschen von überflüssigen Dateien

--- src/specatalog/data_management/multi_file_loader.py
+++
@@ -1,7 +0,0 @@
-import measurement_management as mm
-from specatalog.main import BASE_PATH
-
-
-def multiple_files_to_folder(list_raw_data, fmt, ms_id):
-    for entry in list_raw_data:
-        mm.raw_data_to_folder(entry, fmt, BASE_PATH, ms_id)
